e51648020fb50f07c29cf10f2c18c7300b052174
Belite <batybelite12@gmail.com>
2026-01-19 08:45:29 +0200

    Briefly describe what you changed

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import express from 'express';
 import connectDB from './config/database';
-// import categoryRoutes from './routes/category.routes'
 import router from './routes/category.routes';
 import productrouter from './routes/products.routes';
-// import userRouter from "./routes/user";
-// import {authRouter} from "./routes/auth";
+
 import userRouter from './routes/user.routers';
 import authRouter from './routes/auth.routers';
 import Prouter from './routes/password.router';
@@ -12,13 +10,18 @@ import Prouter from './routes/password.router';
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./config/swagger";
 
+import dotenv from 'dotenv';
 
 
 
 
-
+dotenv.config();
 
 const app  = express();
+const PORT = process.env.PORT || 3000;
+const MONGO_URI= process.env.MONGO_URI;
+const JWT_SECRET = process.env.JWT_SECRET;
+
 app.use(express.json());
 connectDB();
 
@@ -31,7 +34,7 @@ app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
 // app.use("/api/password", Prouter);
 app.use('/api/password', Prouter); // ✅ FIXED
-app.listen(3000, () =>{
-  console.log('server is running on port 3000');
+app.listen(PORT, () =>{
+  console.log('server is running on port ${PORT}');
 
 });
