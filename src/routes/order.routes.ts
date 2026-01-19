ff47b133394035e36e208c46827a3668413b9ad2
Belite <batybelite12@gmail.com>
2026-01-19 11:26:19 +0200

    new changes

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,12 +1,123 @@
-import {Router} from 'express';
-import {createOrder, getOrder, getOrderId, UpdateOrder, deleteOrder} from '../controllers/order.controller';
-
+import { Router } from 'express';
+import { createOrder, getOrder, getOrderId, UpdateOrder, deleteOrder } from '../controllers/order.controller';
 
 const Orouter = Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Orders
+ *   description: Order management
+ */
+
+/**
+ * @swagger
+ * /orders:
+ *   post:
+ *     summary: Create a new order
+ *     tags: [Orders]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               productId:
+ *                 type: string
+ *               quantity:
+ *                 type: integer
+ *               userId:
+ *                 type: string
+ *             required:
+ *               - productId
+ *               - quantity
+ *               - userId
+ *     responses:
+ *       201:
+ *         description: Order created successfully
+ */
 Orouter.post('/', createOrder);
+
+/**
+ * @swagger
+ * /orders:
+ *   get:
+ *     summary: Get all orders
+ *     tags: [Orders]
+ *     responses:
+ *       200:
+ *         description: List of orders
+ */
 Orouter.get('/', getOrder);
+
+/**
+ * @swagger
+ * /orders/{id}:
+ *   get:
+ *     summary: Get order by ID
+ *     tags: [Orders]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Order details
+ *       404:
+ *         description: Order not found
+ */
 Orouter.get('/:id', getOrderId);
-Orouter.put('/:id', UpdateOrder );
+
+/**
+ * @swagger
+ * /orders/{id}:
+ *   put:
+ *     summary: Update an order
+ *     tags: [Orders]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               quantity:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: Order updated
+ *       404:
+ *         description: Order not found
+ */
+Orouter.put('/:id', UpdateOrder);
+
+/**
+ * @swagger
+ * /orders/{id}:
+ *   delete:
+ *     summary: Delete an order
+ *     tags: [Orders]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Order deleted
+ *       404:
+ *         description: Order not found
+ */
 Orouter.delete('/:id', deleteOrder);
+
 export default Orouter;
